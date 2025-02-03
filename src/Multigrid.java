c2989720455c78d62864313fe6bc00eaa32f1991
alexfromsun <113263765+alexfromsun@users.noreply.github.com>
2025-02-03 14:02:34 +0100

    Fixed rhombi orientation

--- src/Multigrid.java
+++ src/Multigrid.java
@@ -502,7 +502,6 @@ class GridTile {
             throw new RuntimeException("Index count does not match");
         }
         GridPoint mainVertex = null;
-
         List<Integer> noDuplicates = new ArrayList<>();
         for (Integer index : indices) {
             int i = noDuplicates.indexOf(index);
@@ -520,11 +519,25 @@ class GridTile {
         int min = Collections.min(noDuplicates);
         int max = Collections.max(noDuplicates);
 
-        mainVertex = vertexList.get(indices.indexOf(min));
+// 0.2 - 0/3 (0) 2/4 (2) type 1
+// 0.4 - 0/3 (3) 1/4 (1) type 2 (reversed)
+// 0.6 - 0/2 (2) 1/4 (4) type 2
+// 0.8 - 0/2 (0) 1/3 (3) type 1 (reversed)
 
-        //.4 .8
-        if (max == 3) {
-            mainVertex = vertexList.get(indices.indexOf(max));
+        if (min == 0 && max == 2) {
+            mainVertex = vertexList.get(indices.indexOf(2));
+        } else if (min == 0 && max == 3) {
+            mainVertex = vertexList.get(indices.indexOf(0));
+        } else if (min == 1 && max == 3) {
+            mainVertex = vertexList.get(indices.indexOf(1));
+        } else if (min == 2 && max == 4) {
+            mainVertex = vertexList.get(indices.indexOf(2));
+        } else if (min == 1 && max == 4) {
+            mainVertex = vertexList.get(indices.indexOf(4));
+        } else {
+            System.out.println("min = " + min);
+            System.out.println("max = " + max);
+            throw new RuntimeException("Unexpected pair of indices");
         }
 
         while (mainVertex != vertexList.get(0)) {
